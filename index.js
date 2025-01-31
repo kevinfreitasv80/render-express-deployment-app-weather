c507cd8b35e20bfdb08e92c2643f9ba94cbff01e
Kevin Freitas Vieira da Silva <kevinfreitasv80@gmail.com>
2025-01-31 18:50:58 -0300

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 4625;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://kevinfreitasv80.github.io",
+    origin: "*",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
@@ -27,44 +27,37 @@ app.post("/", async (req, res) => {
   }
 
   try {
-    const dataWeather = await fetch(url.currentWeather(lat, lon, unit))
-      .then((r) => r.json())
-      .then(async (data) => {
-        let d = await data;
-        let json = {
-          temperature: d.main.temp,
-          localName: d.name,
-          countryName: d.sys.country,
-          codeIcon: d.weather[0].icon,
-          date: new Date(d.dt * 1000),
-          typeWeather: d.weather[0].main,
-          humidity: d.main.humidity,
-          pressure: d.main.pressure,
-          visibility: d.visibility,
-          feels_like: d.main.feels_like,
-          sunrise: new Date(d.sys.sunrise * 1000),
-          sunset: new Date(d.sys.sunset * 1000),
-        };
-        return json;
-      })
-      .catch((e) => `Error in request: ${e}`);
+    const [dataWeather, dataForecast, dataAirPollution] = await Promise.all([
+      fetch(url.currentWeather(lat, lon, unit))
+        .then((r) => r.json())
+        .then((data) => ({
+          temperature: data.main.temp,
+          localName: data.name,
+          countryName: data.sys.country,
+          codeIcon: data.weather[0].icon,
+          date: new Date(data.dt * 1000),
+          typeWeather: data.weather[0].main,
+          humidity: data.main.humidity,
+          pressure: data.main.pressure,
+          visibility: data.visibility,
+          feels_like: data.main.feels_like,
+          sunrise: new Date(data.sys.sunrise * 1000),
+          sunset: new Date(data.sys.sunset * 1000),
+        })),
 
-    const dataForecast = await fetch(url.forecast(lat, lon, unit))
-      .then((r) => r.json())
-      .then(async (dataPromise) => {
-        let list = await dataPromise.list;
-        list = getFormattedDate(list);
-        return list;
-      })
-      .catch((e) => `Error in request: ${e}`);
+      fetch(url.forecast(lat, lon, unit))
+        .then((r) => r.json())
+        .then((data) => getFormattedDate(data.list)),
 
-    const dataAirPollution = await fetch(url.airPollution(lat, lon))
-      .then((r) => r.json())
-      .then(async (data) => {
-        let d = await data.list[0].components;
-        return (list = { pm2_5: d.pm2_5, so2: d.so2, no2: d.no2, o3: d.o3 });
-      })
-      .catch((e) => `Error in request: ${e}`);
+      fetch(url.airPollution(lat, lon))
+        .then((r) => r.json())
+        .then((data) => ({
+          pm2_5: data.list[0].components.pm2_5,
+          so2: data.list[0].components.so2,
+          no2: data.list[0].components.no2,
+          o3: data.list[0].components.o3,
+        })),
+    ]);
 
     return res.status(200).json({
       weather: dataWeather,
